136698b189e4db159c228adec9120bde0c4829a2
vermashanu <insanshanu11@gmail.com>
2022-08-08 11:14:28 -0400

    Add files via upload
    Taking inputs in Java

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,43 +1,29 @@
+import java.util.*;      // Package which contains a lot of inbuilt classes in Java 
+                         // here we are imprting Java's util class and util's class all functions
+
 public class JavaBasics{
     public static void main(String args[]){
-        
-        // 1. boilerplate code
+        // 4. taking inputs in Java
+    Scanner sc = new Scanner(System.in);
+    //String input = sc.next();     // doesn't input spaces and characters after that
+    //System.out.println(input);
+
+    //String input1 = sc.nextLine();
+    //System.out.println(input1);
+
+    //int intInput = sc.nextInt();   // taking an integer input
+    //System.out.println(intInput);
+
+    Boolean bool = sc.nextBoolean();
+    System.out.println(bool);
     /*
-        int a=10;
-        int b=56;
-
-    System.out.println("a");
-    System.out.println(a);
-    System.out.println(b);
-    String name = "Tony Stark";
-    System.out.println(name);
-    a = 22;
-    System.out.println(a);
-    a=b;
-    System.out.println(a);
-
+    More input types:
+    nextByte();
+    nextFloat();
+    nextDouble();
+    nextShort();
+    nextLong();
     */
-
-    // 2. primitive datatypes
-    /*byte by = 5;           // can store upto 256 values only : occupies 1 Byte = 8 bits : -128 to 127
-    System.out.println(by);
-    char c = 'd';         // can only store one character    : 2 Bytes
-    System.out.println(c);
-    boolean var = true;   // occupies 1 Byte
-    int i = 45;           // can store integer values : 4 Bytes
-    long l = 1234356789;  // can store long int values : 8 Bytes
-    short s = 240;        // occupies 2 Bytes 
-
-    float fl = 10.4;      // can store decimal values  : 4 Bytes
-    double db = 12345.67; // can store large decimal values  : 8 Bytes
-    */
-
-    //code to calculate sum of two numbers
-    int a =12;
-    int b =5;
-    int sum = a+b;
-    System.out.println(sum);
-
 
     }
 }
