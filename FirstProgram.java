2db761d4ef042014dbba055bae552c25f82b4f51
vermashanu <insanshanu11@gmail.com>
2022-08-08 11:25:23 -0400

    Add files via upload
    1. Printing outputs in Java
    2. Datatypes
    3. Calculate sum of two numbers

---
+++ FirstProgram.java
@@ -0,0 +1,44 @@
+public class JavaBasics{
+    public static void main(String args[]){
+
+    // 1. printing outputs in Java    
+    /*
+        int a=10;
+        int b=56;
+    System.out.println("a");
+    System.out.println(a);
+    System.out.println(b);
+    String name = "Tony Stark";
+    System.out.println(name);
+    a = 22;
+    System.out.println(a);
+    a=b;
+    System.out.println(a);
+    */
+
+    // 2. primitive datatypes
+
+
+    /*byte by = 5;           // can store upto 256 values only : occupies 1 Byte = 8 bits : -128 to 127
+    System.out.println(by);
+    char c = 'd';         // can only store one character    : 2 Bytes
+    System.out.println(c);
+    boolean var = true;   // occupies 1 Byte
+    int i = 45;           // can store integer values : 4 Bytes
+    long l = 1234356789;  // can store long int values : 8 Bytes
+    short s = 240;        // occupies 2 Bytes 
+    float fl = 10.4;      // can store decimal values  : 4 Bytes
+    double db = 12345.67; // can store large decimal values  : 8 Bytes
+    */
+
+    // 3. sum of two numbers
+    int a =12;
+    int b =5;
+    int sum = a+b;
+    System.out.println(sum);
+
+
+    }
+}
+
+// boilerplate code
