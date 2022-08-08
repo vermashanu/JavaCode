ae6eec51e5ad6eac28135e5b34fdb3117f942a60
vermashanu <insanshanu11@gmail.com>
2022-08-08 11:26:04 -0400

    Add files via upload
    Taking inputs in Java

---
+++ JavaBasics.java
@@ -0,0 +1,31 @@
+import java.util.*;      // Package which contains a lot of inbuilt classes in Java 
+                         // here we are imprting Java's util class and util's class all functions
+
+public class JavaBasics{
+    public static void main(String args[]){
+        // 4. taking inputs in Java
+    Scanner sc = new Scanner(System.in);
+    //String input = sc.next();     // doesn't input spaces and characters after that
+    //System.out.println(input);
+
+    //String input1 = sc.nextLine();
+    //System.out.println(input1);
+
+    //int intInput = sc.nextInt();   // taking an integer input
+    //System.out.println(intInput);
+
+    Boolean bool = sc.nextBoolean();
+    System.out.println(bool);
+    /*
+    More input types:
+    nextByte();
+    nextFloat();
+    nextDouble();
+    nextShort();
+    nextLong();
+    */
+
+    }
+}
+
+
