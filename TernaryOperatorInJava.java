dfde2ef4233fed66ee1f6439ce249f7f9e143626
vermashanu <insanshanu11@gmail.com>
2022-08-09 12:14:22 -0400

    Add files via upload
    Ternary operator :
    variable = condition? statement1 : statement2

---
+++ TernaryOperatorInJava.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class TernaryOperatorInJava{
+    
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        String even = (number%2==0)?"even":"odd";
+        System.out.println(even);
+    
+    }
+    
+    }
