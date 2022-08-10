490c93d69bb7f75046385d922ca17cca2c79df01
vermashanu <insanshanu11@gmail.com>
2022-08-10 13:10:46 -0400

    Add files via upload
    do{
    ---
    ---
    }while(---);

---
+++ DoWhileExample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class DoWhileExample
+{
+    public static void main(String args[]){
+        int i = 1;
+        do{
+            System.out.println("Hello World");
+            i++;
+        }while(i<=10);
+    }
+}
