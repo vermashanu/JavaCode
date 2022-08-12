565de6b48c0943d9b5b9587904f18bc15f0a9646
vermashanu <insanshanu11@gmail.com>
2022-08-12 14:09:55 -0400

    Add files via upload

---
+++ HalfPyramid.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class HalfPyramid {
+    public static void main(String args[]){
+
+        for (int line =1 ; line <=4; line++){
+            for (int number = 1; number <= line ; number++){
+                System.out.print(number);
+            }
+            System.out.println();
+        }
+    }
+}
