52ad5fa261a2474e5b5c3dbc936ff74f8c877af7
vermashanu <insanshanu11@gmail.com>
2022-08-14 12:56:12 -0400

    Add files via upload
    without passing parameters

---
+++ FunctionExample.java
@@ -0,0 +1,9 @@
+public class FunctionExample{
+
+    public static void helloWorld(){
+    System.out.println("Hello World");
+    }
+    public static void main(String args[]){
+        helloWorld();
+    }
+}
